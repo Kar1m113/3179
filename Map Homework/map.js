ebe55b99a162c243e30562dbf796fba3cf8adfa3
Kar1m113 <khaj0001@student.monash.edu>
2023-09-24 01:05:37 +1000

    Finalised HW

--- Map Homework/map.js
+++ Map Homework/map.js
@@ -1,4 +1,4 @@
-var vg_1 = "Map.vg.json";
+var vg_1 = "TESTMAP.json";
 vegaEmbed("#map", vg_1).then(function(result) {
 // Access the Vega view instance
 //(https://vega.github.io/vega/docs/api/view/) as result.view
