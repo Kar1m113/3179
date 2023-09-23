11bd80290757a0290e2325b46e9480e56d8565ee
Kar1m113 <khaj0001@student.monash.edu>
2023-09-23 18:46:05 +1000

    Changes

---
+++ Week 7/exercise1.js
@@ -0,0 +1,22 @@
+a=1;
+b=10;
+aString=a.toString();
+bString=b.toString();
+
+c=a+b;
+d=aString+aString;
+e=bString+aString;
+f=parseInt(e);
+
+
+date=Date();
+firstdate=date.toLocaleString();
+seconddate=date.toDateString();
+console.log(seconddate);
+
+//A JSON file is a open standard file that stores data in key value pairs and arrays. It uses human readable text to store and transmit the data 
+// it consists of attribute value pairs and arrays
+
+// Vega Lite is able to produce a high level representation of visualizations. it encodes mappings from data to graphical marks, as well as the properties
+/*for these marks
+*/
